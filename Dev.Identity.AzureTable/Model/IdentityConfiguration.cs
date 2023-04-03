d7e13743953099aeb3fb3e8fc1c00017cf6a98ea
Steven Swink <stevenswink@gmail.com>
2023-04-03 18:24:11 -0400

    adding property

--- Dev.Identity.AzureTable/Model/IdentityConfiguration.cs
+++ Dev.Identity.AzureTable/Model/IdentityConfiguration.cs
@@ -1,16 +1,26 @@
-﻿namespace Dev.Identity.AzureTable.Model
+﻿using Newtonsoft.Json;
+
+namespace Dev.Identity.AzureTable.Model
 {
+    [JsonObject("identityConfiguration")]
     public class IdentityConfiguration
     {
+        [JsonProperty("tablePrefix")]
         public string TablePrefix { get; set; }
 
+        [JsonProperty("storageConnectionString")]
         public string StorageConnectionString { get; set; }
 
+        [JsonProperty("locationMode")]
+        public string LocationMode { get; set; }
+
+        [JsonProperty("indexTableName")]
         public string IndexTableName { get; set; }
 
+        [JsonProperty("userTableName")]
         public string UserTableName { get; set; }
 
+        [JsonProperty("roleTableName")]
         public string RoleTableName { get; set; }
-
     }
 }
